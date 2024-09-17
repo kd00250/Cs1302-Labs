50e2212d04d6927ee7d4501b1e04918150029173
kd00250 <dearmank412@gmail.com>
2024-09-16 20:37:43 -0400

    Created Project 1
    1)Created project one and set up repo

---
+++ code/project1/Cs2Project1/src/edu/westga/cs1302/project1/model/Food.java
@@ -0,0 +1,11 @@
+package edu.westga.cs1302.project1.model;
+
+/**
+ * Stores and manages information for a single food item.
+ * 
+ * @author CS 1302
+ * @version Fall 2024
+ */
+public class Food {
+
+}
