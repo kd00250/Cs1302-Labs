96198971ef21d4defe010087eb30dad2540359cd
kd00250 <dearmank412@gmail.com>
2024-09-18 18:00:33 -0400

    Connected UI to pantryItme logic
    1)Added logic for addFood method that adds the food input from the user
    2)Added the types for food as an array of strings
    3)Initialized the comboBox to contain the array of strings

--- code/project1/src/edu/westga/cs1302/project1/view/MainWindow.java
+++ code/project1/src/edu/westga/cs1302/project1/view/MainWindow.java
@@ -2,8 +2,11 @@ package edu.westga.cs1302.project1.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import edu.westga.cs1302.project1.model.PantryItem;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -23,21 +26,34 @@ public class MainWindow {
 	private URL location;
 
 	@FXML
-	private ListView<?> food;
+	private ListView<PantryItem> food;
 
 	@FXML
 	private TextField name;
 
 	@FXML
-	private ComboBox<?> type;
+	private ComboBox<String> type;
 
 	@FXML
 	void addFood(ActionEvent event) {
+		String foodName = this.name.getText();
+		String foodType = this.type.getSelectionModel().getSelectedItem();
+		try {
+			PantryItem item = new PantryItem(foodName, foodType);
+			this.food.getItems().add(item);
+		} catch (IllegalArgumentException errorObject) {
+			Alert errorPopup = new Alert(Alert.AlertType.ERROR);
+			errorPopup.setContentText("Unable to create food item " + errorObject.getMessage()
+					+ " Please reenter the information and try again");
+			errorPopup.showAndWait();
+		}
 
 	}
 
 	@FXML
 	void initialize() {
+		String[] types = { "Vegetable", "Meat", "Bread", "Fruit", "Dessert", "Ingredient" };
+		this.type.getItems().addAll(types);
 		assert this.food != null : "fx:id=\"food\" was not injected: check your FXML file 'MainWindow.fxml'.";
 		assert this.name != null : "fx:id=\"name\" was not injected: check your FXML file 'MainWindow.fxml'.";
 		assert this.type != null : "fx:id=\"type\" was not injected: check your FXML file 'MainWindow.fxml'.";
