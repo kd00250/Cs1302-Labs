d44b4c7c9f6d39df499d640489da800e82f0c217
kd00250 <dearmank412@gmail.com>
2024-11-11 19:49:58 -0500

    Add input validation
    1)Added change listener to display error message when the input field is empty or not a number or is a negative number
    2)Added error input label to display input error when regex expression is violated

--- code/Lab 8/src/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ code/Lab 8/src/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -8,39 +8,60 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-/** Codebehind for the MainWindow of the Application.
+/**
+ * Codebehind for the MainWindow of the Application.
  * 
  * @author CS 1302
  * @version Fall 2024
  */
 public class MainWindow {
 
-    @FXML private CheckBox mustIncludeDigits;
-    @FXML private CheckBox mustIncludeLowerCaseLetters;
-    @FXML private CheckBox mustIncludeUpperCaseLetters;
-    @FXML private TextField minimumLength;
-    @FXML private TextArea output;
-    @FXML private Label errorTextLabel;
-    @FXML private Button generatePasswordButton;
-    
-    private ViewModel vm;
-    
-    @FXML
-    void initialize() {
-    	this.vm = new ViewModel();
-    	this.vm.getRequireDigits().bind(this.mustIncludeDigits.selectedProperty());
-    	this.vm.getRequireLowercase().bind(this.mustIncludeLowerCaseLetters.selectedProperty());
-    	this.vm.getRequireUppercase().bind(this.mustIncludeUpperCaseLetters.selectedProperty());
-    	this.minimumLength.setText(this.vm.getMinimumLength().getValue());
-    	this.vm.getMinimumLength().bind(this.minimumLength.textProperty());
-    	
-    	this.output.textProperty().bind(this.vm.getPassword());
-    	this.errorTextLabel.textProperty().bind(this.vm.getErrorText());
-    	
-    	this.generatePasswordButton.setOnAction(
-    			(event) -> { 
-    				this.vm.generatePassword();
-    			} 
-    	);
-    }
+	@FXML
+	private CheckBox mustIncludeDigits;
+	@FXML
+	private CheckBox mustIncludeLowerCaseLetters;
+	@FXML
+	private CheckBox mustIncludeUpperCaseLetters;
+	@FXML
+	private TextField minimumLength;
+	@FXML
+	private TextArea output;
+	@FXML
+	private Label errorTextLabel;
+	@FXML
+	private Label errorInputLabel;
+	@FXML
+	private Button generatePasswordButton;
+
+	private ViewModel vm;
+
+	@FXML
+	void initialize() {
+		this.vm = new ViewModel();
+		this.vm.getRequireDigits().bind(this.mustIncludeDigits.selectedProperty());
+		this.vm.getRequireLowercase().bind(this.mustIncludeLowerCaseLetters.selectedProperty());
+		this.vm.getRequireUppercase().bind(this.mustIncludeUpperCaseLetters.selectedProperty());
+		this.minimumLength.setText(this.vm.getMinimumLength().getValue());
+		this.vm.getMinimumLength().bind(this.minimumLength.textProperty());
+
+		this.output.textProperty().bind(this.vm.getPassword());
+		this.errorTextLabel.textProperty().bind(this.vm.getErrorText());
+
+		this.generatePasswordButton.setOnAction((event) -> {
+			this.vm.generatePassword();
+		});
+
+		this.minimumLength.textProperty().addListener((observable, oldValue, newValue) -> {
+			if (newValue.isBlank()) {
+				this.errorInputLabel.setText("Minimum length cannot be empty.");
+				return;
+			}
+			if (!newValue.matches("^\\d+$")) {
+				this.errorInputLabel.setText("Minimum length must be a positive number");
+			} else {
+				this.errorInputLabel.setText("");
+			}
+
+		});
+	}
 }
--- code/Lab 8/src/edu/westga/cs1302/password_generator/viewmodel/ViewModel.java
+++ code/Lab 8/src/edu/westga/cs1302/password_generator/viewmodel/ViewModel.java
@@ -8,7 +8,9 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
-/** Manages utilizing the model and makes properties available to bind the UI elements.
+/**
+ * Manages utilizing the model and makes properties available to bind the UI
+ * elements.
  * 
  * @author CS 1302
  * @version Fall 2024
@@ -18,28 +20,30 @@ public class ViewModel {
 	private BooleanProperty requireDigits;
 	private BooleanProperty requireLowercase;
 	private BooleanProperty requireUppercase;
-	
+
 	private StringProperty password;
 	private StringProperty errorText;
-	
-    private PasswordGenerator generator;
-	
-	/** Initialize the properties for the viewmodel
+
+	private PasswordGenerator generator;
+
+	/**
+	 * Initialize the properties for the viewmodel
 	 */
 	public ViewModel() {
 		this.minimumLength = new SimpleStringProperty("1");
 		this.requireDigits = new SimpleBooleanProperty(false);
 		this.requireLowercase = new SimpleBooleanProperty(false);
 		this.requireUppercase = new SimpleBooleanProperty(false);
-		
+
 		this.password = new SimpleStringProperty("");
 		this.errorText = new SimpleStringProperty("");
 
-        Random randomNumberGenerator = new Random();
-        this.generator = new PasswordGenerator(randomNumberGenerator.nextLong());
+		Random randomNumberGenerator = new Random();
+		this.generator = new PasswordGenerator(randomNumberGenerator.nextLong());
 	}
 
-	/** Return the minimum length property
+	/**
+	 * Return the minimum length property
 	 * 
 	 * @return the minimum length property
 	 */
@@ -47,7 +51,8 @@ public class ViewModel {
 		return this.minimumLength;
 	}
 
-	/** Return the require digits property
+	/**
+	 * Return the require digits property
 	 * 
 	 * @return the require digits property
 	 */
@@ -55,7 +60,8 @@ public class ViewModel {
 		return this.requireDigits;
 	}
 
-	/** Return the require upper case property
+	/**
+	 * Return the require upper case property
 	 * 
 	 * @return the require upper case property
 	 */
@@ -63,7 +69,8 @@ public class ViewModel {
 		return this.requireUppercase;
 	}
 
-	/** Return the require lower case property
+	/**
+	 * Return the require lower case property
 	 * 
 	 * @return the require lower case property
 	 */
@@ -71,7 +78,8 @@ public class ViewModel {
 		return this.requireLowercase;
 	}
 
-	/** Return the password property
+	/**
+	 * Return the password property
 	 * 
 	 * @return the password property
 	 */
@@ -79,7 +87,8 @@ public class ViewModel {
 		return this.password;
 	}
 
-	/** Return the error text property
+	/**
+	 * Return the error text property
 	 * 
 	 * @return the error text property
 	 */
@@ -87,37 +96,42 @@ public class ViewModel {
 		return this.errorText;
 	}
 
-	/** Generates a password using the minimum length, require digit, require lower case, and require upper case property values.
+	/**
+	 * Generates a password using the minimum length, require digit, require lower
+	 * case, and require upper case property values.
 	 * 
-	 * If a password is successfully generated, the error text property is set to empty string and the password property is set to the password generated.
+	 * If a password is successfully generated, the error text property is set to
+	 * empty string and the password property is set to the password generated.
 	 * 
-	 * If an error is encountered, the password property is set to empty, and the error text property is populated with a message describing the problem.
+	 * If an error is encountered, the password property is set to empty, and the
+	 * error text property is populated with a message describing the problem.
 	 */
 	public void generatePassword() {
-    	int minimumLength = -1;
-    	this.password.setValue("");
-    	
-    	try {
-    		minimumLength = Integer.parseInt(this.minimumLength.getValue());
-    	} catch (NumberFormatException numberError) {
-    		this.errorText.setValue("Invalid Minimum Length: must be a positive integer, but was " + this.minimumLength.getValue());
-    		return;
-    	}
-    	
-    	try {
-    		this.generator.setMinimumLength(minimumLength);
-    	} catch (IllegalArgumentException invalidLengthError) {
-    		this.errorText.setValue("Invalid Minimum Length: " + invalidLengthError.getMessage());
-    		return;
-    	}
-    	
-    	this.generator.setMustHaveAtLeastOneDigit(this.requireDigits.getValue());
-    	this.generator.setMustHaveAtLeastOneLowerCaseLetter(this.requireLowercase.getValue());
-    	this.generator.setMustHaveAtLeastOneUpperCaseLetter(this.requireUppercase.getValue());
-    	
-    	String password = this.generator.generatePassword();
-    	
-    	this.password.setValue(password);
-    }
+		int minimumLength = -1;
+		this.password.setValue("");
+
+		try {
+			minimumLength = Integer.parseInt(this.minimumLength.getValue());
+		} catch (NumberFormatException numberError) {
+			this.errorText.setValue(
+					"Invalid Minimum Length: must be a positive integer, but was " + this.minimumLength.getValue());
+			return;
+		}
+
+		try {
+			this.generator.setMinimumLength(minimumLength);
+		} catch (IllegalArgumentException invalidLengthError) {
+			this.errorText.setValue("Invalid Minimum Length: " + invalidLengthError.getMessage());
+			return;
+		}
+
+		this.generator.setMustHaveAtLeastOneDigit(this.requireDigits.getValue());
+		this.generator.setMustHaveAtLeastOneLowerCaseLetter(this.requireLowercase.getValue());
+		this.generator.setMustHaveAtLeastOneUpperCaseLetter(this.requireUppercase.getValue());
+
+		String password = this.generator.generatePassword();
+
+		this.password.setValue(password);
+	}
 
 }
