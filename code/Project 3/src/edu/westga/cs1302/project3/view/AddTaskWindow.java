1f18d0122aa5820b1276d8ef8af0fd1c741c99ca
kd00250 <dearmank412@gmail.com>
2024-12-03 20:34:30 -0500

    Update GUI to Cancel add task
    1)Added private helper method bind to vm to addTaskWindow
    2)Added cancel set on action to bind to vm method to close window when cancel button is clicked
    3)Added a call to bind to vm method in the mainWidow to make functionality accessible in mainWindow

--- code/Project 3/src/edu/westga/cs1302/project3/view/AddTaskWindow.java
+++ code/Project 3/src/edu/westga/cs1302/project3/view/AddTaskWindow.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * Codebehind for the Add Task Window of the application.
@@ -26,18 +28,17 @@ public class AddTaskWindow {
 	@FXML
 	private TextField titleTextField;
 	
-	public void bindToVM(AddTaskWindowViewModel vm) {
-	
-	}
-
 	@FXML
-	void initialize() {
-		assert addTask != null : "fx:id=\"addTask\" was not injected: check your FXML file 'AddTaskWindow.fxml'.";
-		assert cancel != null : "fx:id=\"cancle\" was not injected: check your FXML file 'AddTaskWindow.fxml'.";
-		assert descriptionTextArea != null
-				: "fx:id=\"descriptionTextArea\" was not injected: check your FXML file 'AddTaskWindow.fxml'.";
-		assert titleTextField != null
-				: "fx:id=\"titleTextField\" was not injected: check your FXML file 'AddTaskWindow.fxml'.";
-
+    private AnchorPane pane;
+	
+	/**Provides bindings for the functionality
+	 * 
+	 * @param vm the vm
+	 */
+	public void bindToVM(AddTaskWindowViewModel vm) {
+	this.cancel.setOnAction((event) -> {
+		Stage stage = (Stage) (this.pane).getScene().getWindow();
+		stage.close();
+	});
 	}
 }
